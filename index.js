2436d0c5ef2998b4434f6e5487b0e29ec7c14eac
Yotam Ginzburg <yotam@presencepg.com>
2017-07-10 23:01:56 -0700

    optimize anonymize function

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 function anonymize (fieldsToAnonymize = [], data = []) {
-  const idToAnonMap = data.reduce((userMap, user, index) => {
-    const uniqueId = getUniqueId(user);
+  const idToAnonMap = {};
 
-    userMap[uniqueId] = 'Anon ' + index;
-
-    return userMap;
-  }, {});
-
-  return data.map(user => {
-    const uniqueId = getUniqueId(user),
+  return data.map((user, index) => {
+    const uniqueId       = getUniqueId(user),
           anonymizedData = fieldsToAnonymize.reduce((anonUser, prop) => {
+            if (!idToAnonMap[uniqueId]) {
+              idToAnonMap[uniqueId] = 'Anon ' + index;
+            }
             anonUser[prop] = idToAnonMap[uniqueId];
             return anonUser;
           }, {});
+
     return Object.assign({}, user, anonymizedData);
   });
 }
 
-function getUniqueId (user) {
+function getUniqueId (user = {}) {
   return user.member_id + '' + user.person_code;
 }
 
